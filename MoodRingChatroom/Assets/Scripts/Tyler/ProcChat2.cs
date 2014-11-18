327c0aa126819fbd66b309dc2672f6ff8ab2a0fc
Jacob Ginsparg <jacobginsparg@mail.com>
2014-11-18 02:26:33 -0500

    Formatting

--- MoodRingChatroom/Assets/Scripts/Tyler/ProcChat2.cs
+++ MoodRingChatroom/Assets/Scripts/Tyler/ProcChat2.cs
@@ -4,62 +4,65 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class ProcChat2 : MonoBehaviour {
+public class ProcChat2 : MonoBehaviour
+{
 
-	int GUIWidth = 350, GUIHeight = 500;
-
-
-	public List<string> chatHistory = new List<string>();
-	private string currentMessage = string.Empty;
-	public Vector2 scrollPosition;
+		int GUIWidth = 350, GUIHeight = 500;
 
 
-	public int sadness = 0;
-	public int happiness = 0;
-	public int anger = 0;
-	public int fear = 0;
-	public int disgust = 0;
-	public int surprise = 0;
+		public List<string> chatHistory = new List<string> ();
+		private string currentMessage = string.Empty;
+		public Vector2 scrollPosition;
 
-	private void OnGUI() {
 
-		if (!Network.isClient) {
-			return;
+		public int sadness = 0;
+		public int happiness = 0;
+		public int anger = 0;
+		public int fear = 0;
+		public int disgust = 0;
+		public int surprise = 0;
+
+		private void OnGUI ()
+		{
+
+				if (!Network.isClient) {
+						return;
+				}
+
+				GUILayout.Space (20);
+
+				GUILayout.BeginHorizontal (GUILayout.Width (GUIWidth));
+
+				currentMessage = GUILayout.TextField (currentMessage);
+
+				if (GUILayout.Button ("Send") || (Input.GetKeyDown (KeyCode.Return))) {
+
+						if (!string.IsNullOrEmpty (currentMessage.Trim ())) {
+								networkView.RPC ("ChatMessage", RPCMode.AllBuffered, new object[] {currentMessage });
+
+								//in here process the current message sent!
+								ProcessChatMessage.ProcessChat (currentMessage);
+
+								currentMessage = string.Empty;
+						}
+
+
+				}
+
+
+				GUILayout.EndHorizontal ();
+
+				scrollPosition = GUILayout.BeginScrollView (scrollPosition, true, false, GUILayout.Width (GUIWidth), GUILayout.Height (Screen.height / 2));
+				for (int i = chatHistory.Count - 1; i >= 0; i--)
+						GUILayout.Label (chatHistory [i]);
+
+				GUILayout.EndScrollView ();
 		}
 
-		GUILayout.Space (20);
-
-		GUILayout.BeginHorizontal (GUILayout.Width (GUIWidth));
-
-		currentMessage = GUILayout.TextField (currentMessage);
-
-		if (GUILayout.Button ("Send") || (Input.GetKeyDown(KeyCode.Return))) {
-
-			if(!string.IsNullOrEmpty(currentMessage.Trim())) {
-				networkView.RPC("ChatMessage", RPCMode.AllBuffered, new object[] {currentMessage });
-
-                //in here process the current message sent!
-                ProcessChatMessage.ProcessChat(currentMessage);
-
-				currentMessage = string.Empty;
-			}
-
-
+		[RPC]
+		public void ChatMessage (string message)
+		{
+				chatHistory.Add (message);
 		}
-
-
-	GUILayout.EndHorizontal();
-
-		scrollPosition = GUILayout.BeginScrollView(scrollPosition, true, false, GUILayout.Width(GUIWidth), GUILayout.Height(Screen.height/2));
-		for (int i = chatHistory.Count - 1; i >= 0; i--)
-		GUILayout.Label(chatHistory[i]);
-
-		GUILayout.EndScrollView();
-}
-
-	[RPC]
-	public void ChatMessage(string message) {
-		chatHistory.Add (message);
-	}
 
 }
