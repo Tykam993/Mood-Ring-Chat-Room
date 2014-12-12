fc86bbd3ebb1aceeec9a2be915053ac53160e640
Jacob Ginsparg <jacobginsparg@mail.com>
2014-12-12 03:32:13 -0500

    Perlin noise maybe

--- MoodRingChatroom/Assets/Scripts/Jacob/NoiseGenerator.cs
+++ MoodRingChatroom/Assets/Scripts/Jacob/NoiseGenerator.cs
@@ -21,6 +21,12 @@ public class NoiseGenerator : MonoBehaviour {
 	// An array of colors that will map to the entire plane
 	private Color[] colorNoise;
 
+	private PerlinNoise perlin;
+
+	public float amp = 1.0f;
+	public float frq = 50.0f;
+	public int oct = 50;
+
 	void Start() {
 		// Set the width and height to the plane's dimensions
 		planeWidth = (int) gameObject.renderer.bounds.size.x;
@@ -31,11 +37,14 @@ public class NoiseGenerator : MonoBehaviour {
 
 		colorNoise = new Color[noiseTex.width * noiseTex.height];
 		gameObject.renderer.material.mainTexture = noiseTex;
+
+		perlin = new PerlinNoise(0);
 	}
 
 	void Update() {
 		// MAKE. SOME. NOISE.
 		GeneratePerlin();
+//		GeneratePerlin (true);
 
 		// Compensates for active/deactive states of emotion
 		if(EmotionModel.CurrentState.y < 0) {
@@ -50,6 +59,8 @@ public class NoiseGenerator : MonoBehaviour {
 		if(rand > 20f || rand < 5f) {
 			change*=-1f;;
 		}
+//		amp++;
+		frq-=0.05f;
 	}
 
 	void GeneratePerlin() {
@@ -71,6 +82,23 @@ public class NoiseGenerator : MonoBehaviour {
 			}
 		}
 		noiseTex.SetPixels(colorNoise);
+		noiseTex.Apply();
+	}
+
+	void GeneratePerlin(bool t) {		
+		float r = 1.0f-EmotionModel.CurrentState.x/-1.0f;
+		float g = 1.0f-EmotionModel.CurrentState.magnitude;
+		float b = 1.0f-EmotionModel.CurrentState.x/1.0f;
+
+		
+		for(float x = 0.0F; x < noiseTex.width; x++) {
+			for(float y = 0.0F; y < noiseTex.height; y++) {
+				float noise = perlin.FractalNoise2D(x,y,oct,frq,1.0f);
+				noise = (noise + 1.0f) * 0.5f;
+//				noiseTex.SetPixel((int)x,(int)y,new Color(noise*r,noise*g,noise*b));
+				noiseTex.SetPixel((int)x,(int)y,new Color(noise, noise, noise, 1.0f));
+			}
+		}
 		noiseTex.Apply();
 	}
 }
